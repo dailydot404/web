719f33e91e56ce5285b29c2211a6a5e70ade72a7
dailydot404 <dailydot404@gmail.com>
2026-06-04 14:27:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.querySelectorAll('.carousel-side').forEach(container => {
     let index = 0;
     const slides = container.querySelector('.slides');
     const total = slides.children.length;
+    
+    // Automatically change slide every 4 seconds
     setInterval(() => {
         index = (index + 1) % total;
         slides.style.transform = `translateX(-${index * 100}%)`;
